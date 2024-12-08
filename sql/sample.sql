efe182c64d41147902e57dc86658c5db2c85babb
Odhiambo-Ojuang <odhiambonigel11@gmail.com>
2024-12-07 23:02:37 -0500

    student login info

--- sql/sample.sql
+++ sql/sample.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Employee (
+CREATE TABLE Employees (
     EmployeeID INT PRIMARY KEY,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
@@ -12,6 +12,28 @@ VALUES
 (111, 'John', 'Doe', 'Swag123' ),
 (222, 'Jane', 'Smith', 'Man123'),
 (333, 'Emily', 'Johnson', 'Girl444');
+
+
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Password VARCHAR(50) NOT NULL
+    
+);
+
+-- Insert sample data into the Employees table
+INSERT INTO Students (StudentID, FirstName, LastName, Password)
+VALUES 
+(999, 'John', 'Moe', 'Swag123' ),
+(432, 'Jane', 'Kith', 'Man123'),
+(373, 'Emily', 'Johnman', 'Girl444');
+
+
+
+ 
+
+
 
 
 
