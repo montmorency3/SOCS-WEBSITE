cbe50f1300ea03991736972c1e6822ad497aa177
montmorency3 <117126340+montmorency3@users.noreply.github.com>
2024-12-10 19:21:36 -0500

    Delete public/assets/javascript/landingpage.js
    renamed to hamburger.js

--- public/assets/javascript/landingpage.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("menu");
-
-
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("active"); 
-
-});
-
