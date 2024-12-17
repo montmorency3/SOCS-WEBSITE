69b0d4651df4e377e46d42ed6aaf7e2ae827cc93
mhydarali <muhammadhydarali@gmail.com>
2024-12-17 14:18:16 -0500

    removed some lines

--- sql/appointments.sql
+++ sql/appointments.sql
@@ -1,19 +1,14 @@
-CREATE TABLE StudentAppointment(
-StudentID INT NOT NULL PRIMARY KEY,
-Professor VARCHAR(50) NOT NULL,
-Appointment JSON NOT NULL,
-FOREIGN KEY (StudentID) REFERENCES StudentLogin(StudentID) ON DELETE CASCADE
+-- Active: 1734462787223@@127.0.0.1@3306@phpmyadmin
+
+CREATE TABLE StudentAppointment (
+    StudentID INT NOT NULL PRIMARY KEY,
+    Professor VARCHAR(50) NOT NULL,
+    Appointment JSON NOT NULL,
+    FOREIGN KEY (StudentID) REFERENCES StudentLogin (StudentID) ON DELETE CASCADE
 );
+
 CREATE TABLE ProfessorAvailability (
-    ProfessorID INT PRIMARY KEY,                 -- Unique ID for the professor
-    Availability JSON NOT NULL,                   -- JSON column to store multiple availability slots
-    FOREIGN KEY (ProfessorID) REFERENCES EmployeeLogin(EmployeeID) ON DELETE CASCADE
+    ProfessorID INT PRIMARY KEY, -- Unique ID for the professor
+    Availability JSON NOT NULL, -- JSON column to store multiple availability slots
+    FOREIGN KEY (ProfessorID) REFERENCES EmployeeLogin (EmployeeID) ON DELETE CASCADE
 );
-
-INSERT INTO ProfessorAvailability (ProfessorID, Availability)
-VALUES
-(111, '[{"Date": "2024-12-02", "StartTime": "10:00:00", "EndTime": "12:00:00", "Location": "MC 103"}, 
-        {"Date": "2024-12-10", "StartTime": "14:00:00", "EndTime": "12:00:00", "Location": "MC 103"}]');
-
-
-
--- sql/equipment.sql
+++ sql/equipment.sql
@@ -1,25 +1,19 @@
--- Active: 1733630281008@@127.0.0.1@3306@phpmyadmin
+-- Active: 1734462787223@@127.0.0.1@3306@phpmyadmin
 -- Active: 1733630281008@@127.0.0.1@3306@test
-CREATE TABLE AvailableEquipment(
-ID INT PRIMARY KEY,
-Equipment VARCHAR(50) NOT NULL,
-Amount INT NOT NULL
+CREATE TABLE AvailableEquipment (
+    ID INT PRIMARY KEY,
+    Equipment VARCHAR(50) NOT NULL,
+    Amount INT NOT NULL
 );
 
-INSERT INTO AvailableEquipment (ID ,Equipment, Amount)
-VALUES 
-(001,'Laptops', 50),
-(002,'Projectors', 7),
-(003,'Keyboards', 30);
+INSERT INTO
+    AvailableEquipment (ID, Equipment, Amount)
+VALUES (001, 'Laptops', 50),
+    (002, 'Projectors', 7),
+    (003, 'Keyboards', 30);
 
-
-CREATE TABLE LoanedEquipment(
+CREATE TABLE LoanedEquipment (
     StudentID INT PRIMARY KEY,
     Equipment JSON NOT NULL,
-    FOREIGN KEY (StudentID) REFERENCES StudentLogin(StudentID)
-    );
-
-
-
-
-
+    FOREIGN KEY (StudentID) REFERENCES StudentLogin (StudentID)
+);
