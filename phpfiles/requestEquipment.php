c42ed3eee626a65689ba9d32dc5342bc238f3c8f
Odhiambo-Ojuang <odhiambonigel11@gmail.com>
2024-12-17 15:11:33 -0500

    equipment done

--- phpfiles/requestEquipment.php
+++ phpfiles/requestEquipment.php
@@ -98,6 +98,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     } else {
         echo "Equipment type not found.";
+        echo "Query: " . $query;
     }
 } else {
     echo "Invalid request.";
