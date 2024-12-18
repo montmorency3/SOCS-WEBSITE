df82f61da25e3e091f9bd6f6c9c96b745367ae5f
Odhiambo-Ojuang <odhiambonigel11@gmail.com>
2024-12-18 14:09:14 -0500

    records student appointments

--- sql/appointments.sql
+++ sql/appointments.sql
@@ -1,8 +1,7 @@
 
 CREATE TABLE StudentAppointment(
 StudentID INT NOT NULL PRIMARY KEY,
-Professor VARCHAR(50) NOT NULL,
-Appointment JSON NOT NULL,
+Appointments JSON NOT NULL,
 FOREIGN KEY (StudentID) REFERENCES StudentLogin(StudentID) ON DELETE CASCADE
 );
 CREATE TABLE ProfessorAvailability (
@@ -19,8 +18,8 @@ VALUES
         {"date": "2024-12-20", "time": "9:00 AM - 11:00 AM", "location": "Room 103", "status": "NB"}
     ]'),
 ( 333, '[
-        {"date": "2024-12-18", "time": "10:00 AM - 12:00 PM", "location": "Room 101", "status": "B"},
-        {"date": "2024-12-19", "time": "2:00 PM - 4:00 PM", "location": "Room 102", "status": "B"},
+        {"date": "2024-12-18", "time": "10:00 AM - 12:00 PM", "location": "Room 101", "status": "NB"},
+        {"date": "2024-12-19", "time": "2:00 PM - 4:00 PM", "location": "Room 102", "status": "NB"},
         {"date": "2024-12-20", "time": "9:00 AM - 11:00 AM", "location": "Room 103", "status": "NB"}
     ]');
 
